31929067b32b2d98bf9f64b67104675395409271
rafahg <r.hgar78@gmail.com>
2020-09-21 17:13:23 +0100

    js implementation started

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 // Selectors
 
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
 //Event Listeners
+
+todoButton.addEventListener("click", addTodo);
+
+
+
+
 
 //Functions
 
+function addTodo(event) {
+    //Prevent form from submitting
+    event.preventDefault();
+    // Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey man';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Checked task button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Eliminate task button.
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    todoDiv.appendChild(trashButton);
+    //APPEND STRUCTURE TO UL LIST ELEMENT.
+    todoList.appendChild(todoDiv);
+}
